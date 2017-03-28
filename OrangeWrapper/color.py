cf5bc9525697b7e9b42f85e527981a36cb3ec967
Jon-Mailes Gr <mail@jonni.it>
2017-03-28 15:42:23 +0200

    f*ck, forgot to delete the old files...

--- OrangeWrapper/color.py
+++
@@ -1,3 +0,0 @@
-class Color():
-    
-    YELLOW = 0
--- OrangeWrapper/http.py
+++
@@ -1,9 +0,0 @@
-import GTAOrange as API
-
-__handlers = {}
-
-def bind(method, url, f = None):
-    pass
-
-def onRequest(method, url, query, body):
-    pass
