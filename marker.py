d06b8a0eea70834dfa1ed210e39d47b0de7ae42a
Jon-Mailes Graeffe <jm.graeffe@jonni.it>
2017-03-31 08:39:07 +0200

    added marker library (untested)

--- marker.py
+++ marker.py
@@ -24,35 +24,63 @@ class Marker():
         self.h = h
         self.r = r
     
-    def delete():
-        pass
+    def delete(self):
+        API.DeleteMarker(self.id)
+        del __pool[self.id]
     
-    def distanceTo(x, y, z):
-        pass
+    def distanceTo(self, x, y, z = None):
+        if z is not None:
+            x1, y1, z1 = self.getPosition()
+            return _world.getDistance(x1, y1, z1, x, y, z)
+        else:
+            x1, y1 = self.getPosition()
+            return _world.getDistance(x1, y1, x, y)
     
-    def getID():
-        pass
+    def getID(self):
+        return self.id
     
-    def getPosition():
-        pass
+    def getPosition(self):
+        return (self.x, self.y, self.z)
     
-    def on(event, cb):
-        pass
+    def on(self, event, cb):
+        if event in __ehandlers[event].keys():
+            self._ehandlers[event].append(_event.Event(cb))
+        else:
+            self._ehandlers[event] = []
+            self._ehandlers[event].append(_event.Event(cb))
     
-    def trigger(event, params):
-        pass
+    def trigger(self, event, *args):
+        if event in self._ehandlers.keys():
+            for handler in self._ehandlers[event]:
+                handler.getCallback()(*args)
     
-def create(x, y, z, h, r, blip = None):
-    pass
-
-def exists(id):
-    pass
+def create(x, y, z, h = 1, r = 1, blip = None):
+    marker = Marker(API.CreateMarkerForAll(x, y, z, h, r), x, y, z, h, r)
+    if blip is not None:
+        marker.blip = _blip.create("Marker", x, y, z)
+    return marker
 
 def getByID(id):
-    pass
+    global __pool
+    
+    if isinstance(id, int):
+        if id in __pool.keys():
+            return __pool[id]
+        return False
+    else:
+        raise TypeError('Marker ID must be an integer')
+
+def getAll():
+    return __pool
 
 def on(event, cb):
-    pass
+    if event in __ehandlers.keys():
+        __ehandlers[event].append(_event.Event(cb))
+    else:
+        __ehandlers[event] = []
+        __ehandlers[event].append(_event.Event(cb))
 
-def trigger(event, params):
-    pass
+def trigger(event, *args):
+    if event in __ehandlers.keys():
+        for handler in __ehandlers[event]:
+            handler.getCallback()(*args)
