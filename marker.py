0ded0f916077c66b3f1720e653d8ab793758510e
Jon-Mailes Graeffe <jm.graeffe@jonni.it>
2017-03-31 11:36:24 +0200

    marker lib: added event global event handlers

--- marker.py
+++ marker.py
@@ -1,5 +1,8 @@
 import __orange__ as API
 from GTAOrange import world
+from GTAOrange import event as _event
+from GTAOrange import vehicle as _vehicle
+from GTAOrange import player as _player
 
 __pool = {}
 __ehandlers = {}
@@ -55,6 +58,8 @@ class Marker():
                 handler.getCallback()(*args)
     
 def create(x, y, z, h = 1, r = 1, blip = None):
+    from GTAOrange import blip as _blip
+    
     marker = Marker(API.CreateMarkerForAll(x, y, z, h, r), x, y, z, h, r)
     if blip is not None:
         marker.blip = _blip.create("Marker", x, y, z)
@@ -84,3 +89,36 @@ def trigger(event, *args):
     if event in __ehandlers.keys():
         for handler in __ehandlers[event]:
             handler.getCallback()(*args)
+
+def _onPlayerEnteredMarker(player_id, marker_id):
+    player = _player.getByID(player_id)
+    marker = getByID(marker_id)
+    
+    trigger("playerentered", player)
+    _player.trigger("enteredmarker", marker)
+
+def _onPlayerLeftMarker(player_id, marker_id):
+    player = _player.getByID(player_id)
+    marker = getByID(marker_id)
+    
+    trigger("playerleft", player)
+    _player.trigger("leftmarker", marker)
+
+def _onVehicleEnteredMarker(vehicle_id, marker_id):
+    vehicle = _vehicle.getByID(vehicle_id)
+    marker = getByID(marker_id)
+    
+    trigger("vehicleentered", vehicle)
+    _vehicle.trigger("enteredmarker", marker)
+
+def _onVehicleLeftMarker(vehicle_id, marker_id):
+    vehicle = _vehicle.getByID(vehicle_id)
+    marker = getByID(marker_id)
+    
+    trigger("vehicleleft", vehicle)
+    _vehicle.trigger("leftmarker", marker)
+    
+API.AddServerEvent(_onPlayerEnteredMarker, "EnterMarker")
+API.AddServerEvent(_onPlayerLeftMarker, "LeftMarker")
+API.AddServerEvent(_onVehicleEnteredMarker, "VehEnterMarker")
+API.AddServerEvent(_onVehicleLeftMarker, "VehLeftMarker")
