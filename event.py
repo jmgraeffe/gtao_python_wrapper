a1730c2e54ed040db21cb773bb6c96ca941dba67
Jon-Mailes Gr <mail@jonni.it>
2017-04-01 18:04:56 +0200

    event lib: added authorship information & docstrings

--- event.py
+++ event.py
@@ -1,22 +1,50 @@
+"""Core class of GTA Orange Python wrapper
+"""
+
+__author__ = "Jon-Mailes Graeffe"
+__copyright__ = "Copyright 2017, Jon-Mailes Graeffe"
+__credits__ = ["GTA Orange team", "Hexaflexagon"]
+__license__ = "Closed-source, freeware"
+__maintainer__ = "Jon-Mailes Graeffe"
+
 __pool = {}
 _current = 0
 
-class Event():
 
+class Event():
+    """Event class
+
+    Attributes:
+        id (int): event id
+    """
     id = None
-    
+
     _cb = None
-    
+
     def __init__(self, cb):
+        """Initializes a new event object.
+
+        Args:
+            cb (function): callback function
+        """
         global _current
-        
+
         self._cb = cb
         self.id = _current
-        
+
         _current += 1
-    
+
     def getCallback(self):
+        """Returns callback function.
+
+        Returns:
+            function: callback function
+        """
         return self._cb
-    
+
     def cancel(self):
+        """Cancels an event.
+
+        TODO: UNIMPLEMENTED!
+        """
         pass
