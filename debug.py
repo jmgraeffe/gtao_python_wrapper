6f185a3aa2688d3e59946048ef1963720b4a11b6
Jon-Mailes Gr <mail@jonni.it>
2017-04-01 18:04:43 +0200

    debug lib: added authorship information & docstrings

--- debug.py
+++ debug.py
@@ -1,7 +1,18 @@
-# dumps every attribute of an object to the console
-# obj = object you want to dump
-# magic = True if you want to output "magic" attributes (like __init__, ...) too
-def dump(obj, magic = False):
+"""Debugging library for the GTA Orange Python wrapper
+"""
+__author__ = "Jon-Mailes Graeffe"
+__copyright__ = "Copyright 2017, Jon-Mailes Graeffe"
+__credits__ = ["GTA Orange team", "Hexaflexagon"]
+__license__ = "Closed-source, freeware"
+__maintainer__ = "Jon-Mailes Graeffe"
+
+def dump(obj, magic=False):
+    """Dumps every attribute of an object to the console.
+
+    Args:
+        obj (any object): object you want to dump
+        magic (bool, optional): True if you want to output "magic" attributes (like __init__, ...)
+    """
     for attr in dir(obj):
         if magic is True:
             print("obj.%s = %s" % (attr, getattr(obj, attr)))
