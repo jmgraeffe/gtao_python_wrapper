82c08b411e982ba28e9081039a1111a061c7feb7
Jon-Mailes Gr <mail@jonni.it>
2017-04-01 18:03:48 +0200

    color lib: added authorship information & docstrings

--- color.py
+++ color.py
@@ -1,3 +1,10 @@
+__author__ = "Jon-Mailes Graeffe"
+__copyright__ = "Copyright 2017, Jon-Mailes Graeffe"
+__credits__ = ["GTA Orange team", "Hexaflexagon"]
+__license__ = "Closed-source, freeware"
+__maintainer__ = "Jon-Mailes Graeffe"
+
 class Color():
-    
+    """Enum-like class with attributes representing all colors which can be used for GTA Orange (not blips!)
+    """
     YELLOW = 0
