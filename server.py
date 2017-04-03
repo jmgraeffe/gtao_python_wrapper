c3caa4917e03b192aca0ddc10e244422aac0c9c7
Jon-Mailes Gr <mail@jonni.it>
2017-04-03 19:13:17 +0200

    server lib: added docstrings

--- server.py
+++ server.py
@@ -1,3 +1,12 @@
+"""Python wrapper for GTA Orange's server functions
+
+Subscribable built-in events:
++========+=========================+==================+
+|  name  | vehicle-local arguments | global arguments |
++========+=========================+==================+
+| unload | ???                     | ???              |
++--------+-------------------------+------------------+
+"""
 import __orange__
 from GTAOrange import event as _event
 
@@ -5,10 +14,21 @@ __ehandlers = {}
 
 
 def broadcast(text):
+    """Broadcasts a message to all players.
+
+    Args:
+        text (str): message string
+    """
     __orange__.Broadcast(text)
 
 
 def on(event, cb):
+    """Subscribes for an event.
+
+    Args:
+        event (string): event name
+        cb (function): callback function
+    """
     if event in __ehandlers.keys():
         __ehandlers[event].append(_event.Event(cb))
     else:
@@ -17,6 +37,12 @@ def on(event, cb):
 
 
 def trigger(event, *args):
+    """Triggers an event.
+
+    Args:
+        event (string): event name
+        *args: arguments
+    """
     if event in __ehandlers.keys():
         for handler in __ehandlers[event]:
             handler.getCallback()(*args)
